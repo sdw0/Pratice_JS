375b66be4b32c7e7e5953365f67f89d21512c0ab
DESKTOP-BQP2EJU\fizz0 <fizz0457@naver.com>
2023-02-24 23:34:39 +0900

    no message

---
+++ Example/dom_test.js
@@ -0,0 +1,12 @@
+document.write("world!");
+var a = document.getElementsByTagName('h2');
+document.write(a[0]);
+
+/*
+document.getElementsByTagName('h2');
+document.getElementById('dw');
+innerHTML ="helo";
+
+
+
+*/
---
+++ Menu/add_button.js
@@ -0,0 +1,40 @@
+var readInput = function() {
+    var input = document.getElementById('input1');
+    document.write(input.value);
+}
+
+
+
+
+
+/*
+var readInput = function() {
+    var input = document.getElementById('input1');
+    console.log(input.value);
+}
+
+
+var btn2 = document.createElement('button');
+btn2.onclick = readInput
+btn2.innerHTML = "Button2";
+var test= document.getElementByID('test);
+test.appendChild(document.createElement('br'));
+test.appendChild(btn2);
+
+
+
+var test = document.getElementById('test');
+var menu = document.createElement('ul');        // ul 태그
+var item1 = document.createElement('li');
+item1.innerHTML = "치킨";
+menu.appendChild(item1);
+var item2 = document.createElement('li');
+item2.innerHTML = "짜장면";
+menu.appendChild(item2);
+
+test.appendChild(menu);
+
+document.write("gege");
+
+
+*/
